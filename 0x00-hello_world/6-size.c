d7851cbce07d7566e6fb6cab126e62b187bb01c3
Laura <laura.caorozco@gmail.com>
2021-02-03 23:33:38 +0000

    Files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,22 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long longlongType;
+	float floatType;
+
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of longint: %zu bytes\n", sizeof(longintType));
+	printf("Size of longlong: %zu byte\n", sizeof(longlongType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	return (0);
+}
