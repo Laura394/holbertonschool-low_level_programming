eb8e14c06d1f16484199041d09abeb4b9181a926
Laura <laura.caorozco@gmail.com>
2021-02-03 23:44:05 +0000

    Files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of char: %zu bytes\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of longint: %zu bytes\n", sizeof(longintType));
-	printf("Size of longlong: %zu byte\n", sizeof(longlongType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long: %zu byte\n", sizeof(longlongType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	return (0);
 }
