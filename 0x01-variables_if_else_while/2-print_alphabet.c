fe812e297261e212f0b8a2845e0575bd7fab6364
Laura <laura.caorozco@gmail.com>
2021-02-05 02:42:18 +0000

    Modified files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int letra = 97;
+
+	while (letra <= 122)
+	{
+		putchar(letra);
+		letra++;
+		}
+	putchar('\n');
+
+	return (0);
+}
