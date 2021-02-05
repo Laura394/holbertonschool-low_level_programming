ed1e28fb0c559d82e37eb21bcc57b8546c7c6c43
Laura <laura.caorozco@gmail.com>
2021-02-05 19:16:13 +0000

    Print comb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,19 +8,18 @@ int main(void)
 {
 		int cero;
 
-		cero= 48;
+		cero = 48;
 		while (cero <= 57)
 		{
-		
-				putchar(cero);
-				if (cero < 57)
-				{
-					putchar(44);
-					putchar(32);
-				
-				}
-				cero++;
+			putchar(cero);
+			if (cero < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			cero++;
 		}
 		putchar(10);
 		return (0);
+
 }
