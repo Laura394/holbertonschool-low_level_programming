dfbe713d64a3bd4ae24e44696e4f613f16ac233b
Laura <laura.caorozco@gmail.com>
2021-02-09 17:21:59 +0000

    Times table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Entry point
- *
- * Return: Always 0 (Success)
+ * print_to_98 - Print numbers n to 98
+ *@n: interger
  */
 void print_to_98(int n)
 {
 	while (n < 98)
-		{
+	{
 		_putchar("%d, ", n);
 		n++;
-		}
+	}
 
 	while (n > 98)
-		{
+	{
 		_putchar("%d, ", n);
 		n--;
-		}
+	}
 
 	if (n == 98)
 		_putchar("%d", n);
 
-		_putchar("\n");
+	_putchar("\n");
 }
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,40 @@
 #include "holberton.h"
 
 /**
- * times_table - Entry point
- *
- * Return: Always 0 (Success)
+* times_table - print multiplication table
  */
+
 void times_table(void)
 {
-	int tabla, numero;
- 
-	for (tabla<=9; tabla++)
-		for (numero=1; numero<=10; numero++)
-			_putchar("%d ',' %d ' ' %d\n", tabla, numero, tabla*numero);
+	int nine;
+	int one;
+	int multi;
+
+	for (nine = 0; nine <= 9; nine++)
+	{
+		for (one = 0; one <= 9; one++)
+		{
+			multi = (nine * one);
+
+			if (one == 0)
+			{
+				_putchar('0' + multi);
+			}
+			else if (multi <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + multi);
+			}
+			else if (multi > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (multi / 10));
+				_putchar('0' + (multi % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
