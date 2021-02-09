8f523bbe609c5e247bd5e3901142826221abada9
Laura <laura.caorozco@gmail.com>
2021-02-09 17:03:43 +0000

    files

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
+
 /**
- *_isalpha - entry point
+ *_isalpha - check if c is a letter
  *@c: is a variable
  * Return: Always 0 (Success)
  */
+
 int _isalpha(int c)
 {
 
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-			return (1);
+		return (1);
 	}
 	else
 	{
-			return (0);
+		return (0);
 	}
-	return ('\n');
 }
 
