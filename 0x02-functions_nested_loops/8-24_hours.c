cf7ee6cd11f3d353c1b3d605ade4cb15b9df3928
Laura <laura.caorozco@gmail.com>
2021-02-09 17:55:13 +0000

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,34 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * jack_bauer - Entry point
- *
- * Return: Always 0 (Success)
+ * jack_bauer - rint every minute of a day
+ * Description: print minutes 00:00 to 23:59
  */
 void jack_bauer(void)
 {
 	int hour = 0;
 	int min = 0;
-	
-	while (hour <= 23 && min <= 59){
-		if (hour <= 9){
-			printf("0");
-		}
-		printf("%d:", hour);
-		if (min <= 9){
-			printf("0");
-		}
-		printf("%d", min);
-		printf("\n");
 
-		if (min == 59){
+	while (hour <= 23 && min <= 59)
+	{
+		if (hour <= 9)
+	{
+			_putchar("0");
+	}
+		_putchar("%d:", hour);
+		if (min <= 9)
+	{
+			_putchar("0");
+	}
+		_putchar("%d", min);
+		_putchar("\n");
+
+		if (min == 59)
+	{
 			hour++;
 			min = 0;
-		}
-		else {
+	}
+		else
+		{
 			min++;
 		}
 	}
