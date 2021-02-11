ca4293fcff1c255ce539ea851c9a1b7b1e22983c
Laura <laura.caorozco@hotmail.com>
2021-02-11 19:28:37 +0000

    Isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _isupper - checks for uppercase character.
+*@c: is a variable
+*Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return(1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return(0);
+	}
+	return(0);
+}
+
