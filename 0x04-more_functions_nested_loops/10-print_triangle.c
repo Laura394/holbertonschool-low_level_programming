01e762ef9560956b638821c563833fbe0e3f4409
Laura <laura.caorozco@hotmail.com>
2021-02-12 04:30:11 +0000

    prints

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_triangle - check the code for Holberton School students.
+ *@size: is a variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+
+
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_square - print a square.
+ *@size: is a variable
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar(35);
+		}
+		if (a != size - 1)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
