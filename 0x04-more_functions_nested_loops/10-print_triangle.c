83a3a57a52c166b27f3bdaf9d76ad78e6b150463
Laura <laura.caorozco@hotmail.com>
2021-02-12 04:44:26 +0000

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,6 +6,18 @@
  */
 void print_triangle(int size)
 {
+	int a;
+	int b;
+	int c;
 
-
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= (size - a); b++)
+			_putchar(' ');
+		for (c = 1; c <= a ; c++)
+		{
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
 }
